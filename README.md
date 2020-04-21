e357a4cfba255cd060865f765d065fbcfd845959
kawam1123 <montblanc.kawamura@gmail.com>
2020-04-22 00:33:00 +0900

    v1.1 レターペアのない手順表(Jack Cai、Graham Siggins)に対応した。

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # showAlgString
-3-Style手順表からコピペ用の文字列を出力するためのGoogle Apps Scriptsです。
+3-Style手順表からコピペ用の文字列を出力するためのGoogle Apps Scriptです。自分の使っている手順とレターペアをコピーして、シェアしたいときに便利なスクリプトです。
 
-## 前提条件
-- Google スプレッドシート上に3-Style手順表があること
+## 動作要件
+### 前提
+- Google スプレッドシート上に3-Style手順表がある
+- エッジおよびコーナーのどちらでも動作する
+- シート名にバッファが書いてある
+- PC上のブラウザからアクセスしている (iPhoneなどのスプレッドシートアプリではスクリプトを実行できない)
+
+### 動作確認済みの手順表
+- [まっさん手順 – Google Sheets](https://docs.google.com/spreadsheets/d/1mHlpvaKb9Emi1ach6vsMcQwBY4mNSJ0ncn5fnVOzojE/edit#gid=337645627)
+- [Graham’s Full 3Style Comm Series + more](https://docs.google.com/spreadsheets/d/1-AnKGJMHN3SAOcZxem3XJ5tBm7Dk1dTRcZ7KcXYbGP4/)
+- [Jack’s Comms – Google Sheets](https://docs.google.com/spreadsheets/d/1yl5Xwn2aDpXyHA0Rx1OUrDzec8nbg18wxis3I4uDoIw/edit)
 
 ## インストール方法
 1. Googleスプレッドシートの **[ツール]>[スクリプトエディタ]** を選択する
@@ -30,5 +39,12 @@
 
 **Have a nice speedcubing!**
 
+## バージョン履歴
+### v1.0
+- リリース。まっさんの手順表に対応した。
+### v1.1
+- レターペアのない手順表(Jack Cai、Graham Siggins)に対応した。
+
 ## 謝辞
-素晴らしい3-Styleを作成してくださった先人の成果に感謝します！
+素晴らしい3-Style手順表を作成してくださった先人の成果に感謝します！
+
--- showAlgString.js
+++ showAlgString.js
@@ -12,12 +12,17 @@ function onOpen() {
     var target = sheet.getCurrentCell() //現在のセルを指定する
     var alg = target.getValue(); //アルゴリズムの文字列を取得する　例： [U', R' D' R]
     //var buffer = 'UFR' //バッファを指定する。とりあえず固定値。
-    var buffer = sheet.getName() //シート名にバッファが書いてあると信じてシート名を取得
+    var buffer = sheet.getName().replace('Corners','').replace('Edges','').replace('corners','').replace('edges','').replace(' ',''); //シート名にバッファが書いてあると信じてシート名を取得、cornersやedgesは消去
     var sticker_2nd = sheet.getRange(1, target.getColumn()).getValue(); //例："み (RDF)"
     var sticker_3rd = sheet.getRange(target.getRow(), 1).getValue(); //例："い (UFL)"
-    var letters = getSticker(sticker_2nd)[0]+getSticker(sticker_3rd)[0]; //レターペアのみ取り出す　例："みい"
-    var output_alg = buffer + ' ' + getSticker(sticker_2nd)[1] + ' ' + getSticker(sticker_3rd)[1] + ' ' + alg; //例： UFR RDF UFL
-    var output = letters+' '+output_alg; //例："みい UFR RDF UFL [U', R' D' R]"
+    
+    if(!getSticker(sticker_2nd)[1]){//もしレターペアのない手順表ならばならば
+      var output = buffer + ' ' + getSticker(sticker_2nd)[0] + ' ' + getSticker(sticker_3rd)[0] + ' ' + alg; //例： UFR RDF UFL
+    }else{//もしレターペアつきの手順表ならば
+      var letters = getSticker(sticker_2nd)[0]+getSticker(sticker_3rd)[0]; //レターペアのみ取り出す　例："みい"
+      var output_alg = buffer + ' ' + getSticker(sticker_2nd)[1] + ' ' + getSticker(sticker_3rd)[1] + ' ' + alg; //例： UFR RDF UFL
+      var output = letters+' '+output_alg; //例："みい UFR RDF UFL [U', R' D' R]"
+    }
     Browser.msgBox(output)   
   }
   
