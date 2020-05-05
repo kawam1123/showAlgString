d3187b66aae08a83d52f37d452c543954be4904d
kawam1123 <montblanc.kawamura@gmail.com>
2020-05-05 11:06:01 +0900

    v1.4 (コミューテータ表記の展開に対応)

--- showAlgString.js
+++ showAlgString.js
@@ -1,5 +1,5 @@
 //Name: showAlgString.js
-//Version: 1.3 (2020/04/26)
+//Version: 1.4 (2020/05/05)
 //Author: kawam1123
 
 function onOpen() {
@@ -8,98 +8,196 @@ function onOpen() {
   menu.addItem('レターペア+ステッカー+手順', 'showAlgStringFull');
   menu.addItem('ステッカー+手順', 'showAlgStringSummary');
   menu.addItem('手順', 'showAlgStringMin');
-  menu.addToUi();
-}
+  menu.addSeparator();
+  menu.addItem('レターペア+ステッカー+手順+展開', 'showAlgStringFullwithDecom');
+  menu.addItem('ステッカー+手順+展開', 'showAlgStringSummarywithDecom');
+  menu.addItem('手順+展開', 'showAlgStringMinwithDecom');
+  menu.addItem('展開', 'showAlgStringMinwithDecomOnly');
+  
+    menu.addToUi();
+  }
 
 function showAlgStringFull(){
-//出力形式：レターペア、ステッカー、手順
-showAlgStringsRange(option=1);
+  //出力形式：レターペア、ステッカー、手順
+  showOutputString(showAlgStringsRange(format=1));
 }
 
 function showAlgStringSummary(){
-//出力形式：ステッカー、手順
-showAlgStringsRange(option=2);
+  //出力形式：ステッカー、手順
+  showOutputString(showAlgStringsRange(format=2));
 }
 
 function showAlgStringMin(){
-//出力形式：ステッカー、手順
-showAlgStringsRange(option=3);
+  //出力形式：ステッカー、手順
+  showOutputString(showAlgStringsRange(format=3));
 }
 
-function showAlgStringsRange(option){
-//複数の選択された範囲について、文字列を取得し、メッセージを出す
-var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-var sheet=spreadsheet.getActiveSheet();
-var targets = sheet.getActiveRangeList().getRanges(); //選択されたセルのリストを取得
-console.log("処理開始");
-
-//出力文字列を初期化する
-outputString = "";
-
-for (var target in targets) {
-  //選択された複数の範囲についてループ処理する
-  //複数の範囲がある場合、それぞれの出力結果の間には空行を挿入する
-  console.log("次の範囲を処理する："+targets[target].getA1Notation());
-  outputString　+= getAlgString(sheet,targets[target],option) + "\\n"
+function showAlgStringFullwithDecom(){
+  //出力形式：レターペア、ステッカー、手順、展開
+  showOutputString(showAlgStringsRange(format=1,decompress=true));
 }
 
-//画面上に出力する。あとはコピペで頑張ってくれ。
-Browser.msgBox(outputString)  
+function showAlgStringSummarywithDecom(){
+  //出力形式：ステッカー、手順、展開
+  showOutputString(showAlgStringsRange(format=2,decompress=true));
 }
 
-function getAlgString(sheet, target, option) {
+function showAlgStringMinwithDecom(){
+  //出力形式：手順、展開
+  showOutputString(showAlgStringsRange(format=3,decompress=true));
+}
+
+function showAlgStringMinwithDecomOnly(){
+  //出力形式：展開
+  showOutputString(showAlgStringsRange(format=3,decompress=false,decomonly=true));
+}
+
+function showAlgStringsRange(format=1,decompress=false,decomonly=false){
+  //複数の選択された範囲について、文字列を取得し、メッセージを出す
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet=spreadsheet.getActiveSheet();
+  var targets = sheet.getActiveRangeList().getRanges(); //選択されたセルのリストを取得
+  console.log("処理開始");
+  
+  //出力文字列を初期化する
+  outputString = "";
+  
+  for (var target in targets) {
+    //選択された複数の範囲についてループ処理する
+    //複数の範囲がある場合、それぞれの出力結果の間には空行を挿入する
+    console.log("次の範囲を処理する："+targets[target].getA1Notation());
+    outputString　+= getAlgString(sheet,targets[target],format,decompress,decomonly) + "\\n"
+  }
+  
+  return outputString;
+}
+
+function getAlgString(sheet, target, format, decompress=false, decomonly=false) {
 //範囲内で全てのセルについて処理をする
-//optionで出力形式を変える
+//formatで出力形式を変える
 //  1:レターペア、ステッカー、手順 例："みい UFR RDF UFL [U', R' D' R]"
 //  2:ステッカー、手順 例："UFR RDF UFL [U', R' D' R]"
 //  3:手順 例："[U', R' D' R]"
 //  targetはRangeである。Range内には複数の手順が含まれている場合がある。
-//出力文字列を初期化する
-outputString = "";
-//Rangeの範囲内を繰り返し処理する
-for(var row = 1; row <= target.getNumRows(); row++) {
-  for (var column = 1; column <= target.getNumColumns(); column++) {
-    targetCell = target.getCell(row, column); //各セルを特定する
-    targetCellValue = targetCell.getValue(); //各セルの文字列を取得する
-    console.log("処理対象："+targetCellValue+"\n");
-    if(targetCellValue){//空白文字でないなら処理する
-      console.log("個別セルの処理に入る");
-      outputString += generateAlgString(sheet, targetCell, option) + '\\n'
+  //出力文字列を初期化する
+  outputString = "";
+  //Rangeの範囲内を繰り返し処理する
+  for(var row = 1; row <= target.getNumRows(); row++) {
+    for (var column = 1; column <= target.getNumColumns(); column++) {
+      targetCell = target.getCell(row, column); //各セルを特定する
+      targetCellValue = targetCell.getValue(); //各セルの文字列を取得する
+      console.log("処理対象："+targetCellValue+"\n");
+      if(targetCellValue){//空白文字でないなら処理する
+        console.log("個別セルの処理に入る");
+        outputString += generateAlgString(sheet, targetCell, format, decompress, decomonly) + '\\n'
+      }
     }
   }
+ 
+  return outputString  
 }
 
-return outputString  
-}
 
-
-function generateAlgString(sheet, targetCell, option){
+function generateAlgString(sheet, targetCell, format, decompress=false, decomonly){
 //特定のセルtargetCell(Range)を与えると出力用の文字列を生成する
 
-var alg = targetCell.getValue(); //アルゴリズムの文字列を取得する　例： [U', R' D' R]
-//var buffer = 'UFR' //バッファを指定する。とりあえず固定値。
-var buffer = sheet.getName().replace('Corners','').replace('Edges','').replace('corners','').replace('edges','').replace('Comms','').replace(' ',''); //シート名にバッファが書いてあると信じてシート名を取得、cornersやedgesは消去
-var sticker_2nd = sheet.getRange(1, targetCell.getColumn()).getValue(); //例："み (RDF)"
-var sticker_3rd = sheet.getRange(targetCell.getRow(), 1).getValue(); //例："い (UFL)"
-
-if(!getSticker(sticker_2nd)[1]){//もしレターペアのない手順表ならばならば
-  var output = buffer + ' ' + getSticker(sticker_2nd)[0] + ' ' + getSticker(sticker_3rd)[0] + ' ' + alg; //例： UFR RDF UFL
-}else{//もしレターペアつきの手順表ならば
-  var letters = getSticker(sticker_2nd)[0]+getSticker(sticker_3rd)[0]; //レターペアのみ取り出す　例："みい"
-  var output_alg = buffer + ' ' + getSticker(sticker_2nd)[1] + ' ' + getSticker(sticker_3rd)[1] + ' ' + alg; //例： UFR RDF UFL
+  var alg = targetCell.getValue(); //アルゴリズムの文字列を取得する　例： [U', R' D' R]
   
-  //出力形式で分岐
-  if(option==1) var output = letters+' '+output_alg; //例："みい UFR RDF UFL [U', R' D' R]"
-  if(option==2) var output = output_alg; //例："UFR RDF UFL [U', R' D' R]"
-  if(option==3) var output = alg; //例："[U', R' D' R]"
-}
-
-console.log("出力文字列："+output+"\\n");
-return output;
+  if(decomonly){//展開のみオプションが有効であるならば、展開のみ出力
+    output = showAlgDecompressionSimple(alg);
+    console.log("出力文字列(decomonly)："+output+"\n");
+    return output;
+  }
+  
+  //var buffer = 'UFR' //バッファを指定する。とりあえず固定値。
+  var buffer = sheet.getName().replace('Corners','').replace('Edges','').replace('corners','').replace('edges','').replace(' ',''); //シート名にバッファが書いてあると信じてシート名を取得、cornersやedgesは消去
+  var sticker_2nd = sheet.getRange(1, targetCell.getColumn()).getValue(); //例："み (RDF)"
+  var sticker_3rd = sheet.getRange(targetCell.getRow(), 1).getValue(); //例："い (UFL)"
+  
+  if(!getSticker(sticker_2nd)[1]){//もしレターペアのない手順表ならばならば
+    var output = buffer + ' ' + getSticker(sticker_2nd)[0] + ' ' + getSticker(sticker_3rd)[0] + ' ' + alg; //例： UFR RDF UFL
+  }else{//もしレターペアつきの手順表ならば
+    var letters = getSticker(sticker_2nd)[0]+getSticker(sticker_3rd)[0]; //レターペアのみ取り出す　例："みい"
+    var output_alg = buffer + ' ' + getSticker(sticker_2nd)[1] + ' ' + getSticker(sticker_3rd)[1] + ' ' + alg; //例： UFR RDF UFL
+    
+    //出力形式で分岐
+    if(format==1) var output = letters+' '+output_alg; //例："みい UFR RDF UFL [U', R' D' R]"
+    if(format==2) var output = output_alg; //例："UFR RDF UFL [U', R' D' R]"
+    if(format==3) var output = alg; //例："[U', R' D' R]"
+  }
+  
+  if(decompress){ //コミューテータ表記の展開オプションが有効であるならば
+    output += " " + showAlgDecompressionSimple(alg);
+  }
+  
+    
+  console.log("出力文字列："+output+"\n");
+  return output;
 }
 
 //サブ関数：レターペアとステッカーに分解して配列を返す
 function getSticker(sticker_raw){
-var sticker_string = sticker_raw.replace('(','').replace(')','').split(' ');
-return sticker_string
+  var sticker_string = sticker_raw.replace('(','').replace(')','').split(' ');
+  return sticker_string
+}
+
+function showOutputString(outputString){
+  //画面上に出力する。あとはコピペで頑張ってくれ。
+  Browser.msgBox(outputString)   
+}
+
+function showAlgDecompressionSimple(inputString = "[U', R' D R]"){
+  //inputString = showAlgStringsRange(format=3);
+  if(inputString.match(/\:/)){ //セットアップあり
+    str = inputString.replace(/(\[|\]|\n)/g,"").split(": ");
+    var decom = [str[0], decompressComm(str[1]), reverseAlg(str[0])];
+    output = decom.join(" ");
+    Logger.log("decom: ",decom);
+  }else if(inputString.match(/\,/)){ //セットアップなし
+    str = inputString.replace(/(\[|\]|\n)/g,"");
+    output = decompressComm(str);
+  }else{ //特殊手順
+    output = inputString.replace(/(\[|\]|\n)/g,"");
+  }
+  Logger.log(output);
+  //showOutputString(output); 
+  return output;
+}
+
+function reverseAlg(input){
+//手順を逆手順に変換する
+  reversed = input.split(' ').reverse();
+  for (var i = 0, len = reversed.length; i < len; ++i) {
+    reversed[i] = reverseMove(reversed[i]);
+  }
+  result = reversed.join(" ");
+  return result;
+  //Logger.log([input,result]);
+}
+    
+function reverseMove(move){
+    //単一ムーブを逆手順に変換する
+    //R -> R'
+    //U2 -> U2
+    //D' -> D
+    lastLetter = move.slice(-1);
+    if(lastLetter == "'"){
+      output = move.replace("'","");
+    }else if(lastLetter == "2"){
+      //do nothing
+    }else{
+      output = move + "'";
+    }
+  //Logger.log([move,output]);
+    return output;
+}
+
+function decompressComm(move){
+  //「R U R', D」のような文字列を期待する
+  arr=move.split(", ");
+  arr.push(reverseAlg(arr[0]));
+  arr.push(reverseAlg(arr[1]));
+  output=arr.join(" ");
+  Logger.log([move,output]);
+  return output;  
 }
